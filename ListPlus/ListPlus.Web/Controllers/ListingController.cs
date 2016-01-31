ec4a2ee880b416d48974213b1860f9148b066450
kevstutorials <kacarrier@msn.com>
2016-01-30 23:19:30 -0500

    Commit

---
+++ ListPlus/ListPlus.Web/Controllers/ListingController.cs
@@ -0,0 +1,30 @@
+﻿using ListPlus.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ListPlus.Web.Controllers
+{
+    public class ListingController : Controller
+    {
+        // GET: Listing/List
+        public ActionResult List()
+        {
+            /* 
+             *   Hard Coded Data for list using ListingModel
+             *
+             */
+            var list = new List<ListingModel>
+            {
+                new ListingModel {Id = 1, Title = "Food"},
+                new ListingModel {Id = 2, Title = "Hardware"},
+                new ListingModel {Id = 3, Title = "Vacation"}
+            };
+
+
+            return View(list);
+        }
+    }
+}
---
+++ ListPlus/ListPlus.Web/Models/ListingModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ListPlus.Web.Models
+{
+    public class ListingModel
+    {
+        //Item ID
+        public int Id { get; set; }
+        //Item Title
+        public string Title { get; set; }
+    }
+}
